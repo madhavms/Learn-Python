7c0fc08d6e73783e8933c42e5bab00d25c15efb5
Madhav Manohar S <madhavmanohar.s@gmail.com>
2020-09-20 19:26:23 +0530

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,9 @@
+#List Comprehension
+#Alternate to for loops
+#More concise redeable efficient and mimic functional programming style
+#Used to apply a method to all or specific elements of a list
+#Filter elements of a list satisfying specific criteria
+
+x = [1,2,3,4]
+y = [i**2 for i in x] 
+print(y)
